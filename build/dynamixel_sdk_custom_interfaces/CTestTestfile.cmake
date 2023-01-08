7c3eadd7187f22058e8683f05901a1746fd6271d
PVBrodersen <phbro19@student.sdu.dk>
2023-01-08 13:07:23 +0100

    try git pull --recurse-submodules

--- build/dynamixel_sdk_custom_interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philip/Embedded_programming/Embedded_systems_final_clean/DynamixelSDK/dynamixel_sdk_custom_interfaces
-# Build directory: /home/philip/Embedded_programming/Embedded_systems_final_clean/build/dynamixel_sdk_custom_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("dynamixel_sdk_custom_interfaces__py")
