33f146ef78db258df7d2cebbbdcc0fbbc035bf67
PVBrodersen <phbro19@student.sdu.dk>
2023-01-08 10:28:27 +0100

    dependencies

---
+++ build/dynamixel_sdk/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/philip/Embedded_programming/Embedded_systems_final_clean/DynamixelSDK/dynamixel_sdk
+# Build directory: /home/philip/Embedded_programming/Embedded_systems_final_clean/build/dynamixel_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
