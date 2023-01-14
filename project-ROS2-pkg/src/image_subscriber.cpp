69a86629da1c25882b7d22ca890efc9fa0bdaeed
Etonado <erik-winkler@outlook.de>
2023-01-14 13:02:24 +0100

    Synq message

--- project-ROS2-pkg/src/image_subscriber.cpp
+++ project-ROS2-pkg/src/image_subscriber.cpp
@@ -3,12 +3,13 @@
 
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include "example_interfaces/srv/example.hpp"
 
 #include <bram_uio.h>
 #include <../drivers/nn_inference_v1_0/xnn_inference.h>
 
 BRAM bram0(0,64000);
-XNn_Inference NN;
+
 const int target_width = 32;
 const int target_height = 18;
 
@@ -23,19 +24,22 @@ class ImageSubscriber : public rclcpp::Node
 			camera_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
 					"/image_raw",
 					10,
-					std::bind(&ImageSubscriber::onImageMsg, this, std::placeholders::_1)
-			);
-			XNn_inference_Initialize(&NN,'ai');
-			
-_			
+					std::bind(&ImageSubscriber::onImageMsg, this, std::placeholders::_1));
+
+			publisher_ = this->create_publisher<std_msgs::msg::Empty>("synch", 10);
 
 		}
+	
+
 
 	private:
 		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subscription_;
+		rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
 
 		void onImageMsg(const sensor_msgs::msg::Image::SharedPtr msg) {
 			RCLCPP_INFO(this->get_logger(), "Received image!");
+
+			auto message = std_msgs::msg::Empty();
 
 			cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
 			cv::Mat img = cv_ptr->image;
@@ -54,12 +58,13 @@ _
 				}
 				
 			}
-			
-			XNn_inference_Start(&NN)
-
 
 			RCLCPP_INFO(this->get_logger(), "Successfully loaded image");
+
+			publisher_->publish(message);
 		}
+
+
 
 };
 
--- project-ROS2-pkg/src/motor_ctrl_publisher.cpp
+++ project-ROS2-pkg/src/motor_ctrl_publisher.cpp
@@ -7,6 +7,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <msg/set_position.hpp>
+#include "std_msgs/msg/empty.hpp"
 
 #include <../drivers/nn_inference_v1_0/xnn_inference.h>
 
@@ -24,8 +25,13 @@ class MotorPublisher : public rclcpp::Node
         MotorPublisher():Node("motor_publisher"), count_(0)
         {
             publisher_ = this->create_publisher<dynamixel_sdk_custom_interfaces::msg::SetPosition>("motor_ctrl",10);
+            //option 1: timer based writing
             timer_ = this->create_wall_timer(
                 500ms, std::bind(&MotorPublisher::timer_callback, this));    
+            //option 2: synch message based callback
+            subscription_ = this->create_subscription<std_msgs::msg::Empty>(
+            "synch", 10, std::bind(&MotorPublisher::synch_callback, this, _1));
+            
             
             XNn_inference_Initialize(&NN,'ai');
         }
@@ -44,6 +50,67 @@ class MotorPublisher : public rclcpp::Node
         float myNum[] = {o1,o2};
         return myNum;
     }
+
+    void synch_callback()
+    {
+    RCLCPP_INFO(this->get_logger(), "Start Neural Network Processing");
+          auto message = dynamixel_sdk_custom_interfaces::msg::SetPosition();
+
+      std::string joint_1 = '2';
+      std::string joint_2 = '3';
+
+      float angle_1 = 0.0;
+      float angle_2 = 0.0;
+
+      int pose = 0;
+
+      while(!XNn_inference_IsDone(&NN)){}
+
+      pose = (int)XNn_inference_Get_return(&NN);
+      
+      switch (pose)
+      {
+      case 0: // 
+        /* code */
+        x = 2*sqrt(2);
+        y = 2*sqrt(2); 
+        invres = inverse(x,y);
+        break;
+      case 1:
+        x = 1.00;
+        y = 1.00;
+        invres = inverse(x,y);
+        break;
+      case 2:
+        x = 0.0;
+        y = 2.0;
+        invres = inverse(x,y);
+        break;
+      case 3:
+        x = 1.00;
+        y = 1.00
+        invres = inverse(x,y);
+        break;
+      case 4:
+        x = 2.0;
+        y = 0.0;
+        invres = inverse(x,y);
+        break;  
+      default:
+        break;
+      }
+
+      // publish message for first joint
+      message.data = joint_1 + std::to_string(invres[0]);
+      publisher_->publish(message);
+
+      // publish message for second joint
+      message.data = joint_2 + std::to_string(invres[1]);
+      publisher_->publish(message);
+    }
+
+    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr subscription_;
+
 
     void timer_callback()
     {
@@ -111,6 +178,7 @@ class MotorPublisher : public rclcpp::Node
     rclcpp::Publisher<dynamixel_sdk_custom_interfaces::msg::SetPosition>::SharedPtr publisher_;
     size_t count_;
 };
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
